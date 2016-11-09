9b3d5d4e461334cd2ea36f141df9d27606fc3f71
Miroslav Jonas <meeroslav@yahoo.com>
2016-11-09 17:46:36 +0100

    Optimize code

--- src/index.js
+++ src/index.js
@@ -7,57 +7,10 @@ var path = require('path');
 var PLUGIN_NAME = 'json-config';
 
 module.exports = function(fileName, ENVIRONMENT_DEFINITION) {
-    if (!fileName) {
-        throw new PluginError(PLUGIN_NAME, 'Missing fileName');
-    }
 
     var firstFile = null;
     var data = {};
     var skipConversion = false;
-
-    /**
-     * Insert property on correct location
-     * @param parent
-     * @param env
-     * @param destination
-     */
-    function insertProperty(parent, env, destination) {
-        if (parent && parent[env]) {
-            Object.keys(parent[env]).forEach(function(prop){
-                // don't override, only copy if doesn't exist
-                destination[prop] = destination[prop] || parent[env][prop];
-            });
-        }
-    }
-
-    /**
-     * Combine json data based on environment order
-     * @param data
-     * @param environmentOrder
-     */
-    function combineJSONData(data, environmentOrder) {
-        var result = {};
-
-        // iterate through all configFiles
-        Object.keys(data).forEach(function (configName) {
-            if (!environmentOrder) {
-                result[configName] = data[configName];
-                return;
-            }
-            if (configName.indexOf('.local') !== -1) {
-                return; // skip local ones in here, we'll handle them later
-            }
-            // set config wrapper
-            result[configName] = {};
-            // iterate through environment order
-            ENVIRONMENT_DEFINITION[environmentOrder].forEach(function(env){
-                // check for locals first
-                insertProperty(data[configName + '.local'], env, result[configName]);
-                insertProperty(data[configName], env, result[configName]);
-            });
-        });
-        return new Buffer(JSON.stringify(result));
-    }
 
     /**
      * Get name based on environment
@@ -110,27 +63,85 @@ module.exports = function(fileName, ENVIRONMENT_DEFINITION) {
 
             if (ENVIRONMENT_DEFINITION) {
                 Object.keys(ENVIRONMENT_DEFINITION).forEach(function (environmentOrder) {
-                    var newFile = new File({
-                        cwd: cwd,
-                        base: base,
-                        path: path.join(base, composeNewName(fileName, environmentOrder)),
-                        contents: combineJSONData(data, environmentOrder)
-                    });
-                    this.push(newFile);
+                    pushFile(this, cwd, base, environmentOrder);
                 }, this);
             } else {
-                var newFile = new File({
-                    cwd: cwd,
-                    base: base,
-                    path: path.join(base, fileName),
-                    contents: combineJSONData(data)
-                });
-                this.push(newFile);
+                pushFile(this, cwd, base);
             }
             return cb();
 
         }
         cb();
+    }
+
+    /**
+     * Push combined files into a stream
+     * @param stream
+     * @param cwd
+     * @param base
+     * @param environmentOrder
+     */
+    function pushFile(stream, cwd, base, environmentOrder) {
+        var newFile = new File({
+            cwd: cwd,
+            base: base,
+            path: path.join(base, environmentOrder ? composeNewName(fileName, environmentOrder) : fileName),
+            contents: combineJSONData(data, environmentOrder)
+        });
+        stream.push(newFile);
+    }
+
+    /**
+     * Combine json data based on environment order
+     * @param data
+     * @param environmentOrder
+     */
+    function combineJSONData(data, environmentOrder) {
+        var result = {};
+
+        // iterate through all configFiles
+        Object.keys(data).forEach(function (configName) {
+            if (!environmentOrder) {
+                result[configName] = data[configName];
+                return;
+            }
+            if (configName.indexOf('.local') !== -1) {
+                return; // skip local ones in here, we'll handle them later
+            }
+            // set config wrapper
+            result[configName] = {};
+            // iterate through environment order
+            ENVIRONMENT_DEFINITION[environmentOrder].forEach(function(env){
+                // check for locals first
+                insertProperty(data[configName + '.local'], env, result[configName]);
+                insertProperty(data[configName], env, result[configName]);
+            });
+        });
+
+        /**
+         * Insert property on correct location
+         * @param parent
+         * @param env
+         * @param destination
+         */
+        function insertProperty(parent, env, destination) {
+            if (parent && parent[env]) {
+                Object.keys(parent[env]).forEach(function(prop){
+                    // don't override, only copy if doesn't exist
+                    destination[prop] = destination[prop] || parent[env][prop];
+                });
+            }
+        }
+
+        /**
+         * Run plugin
+         */
+
+        if (!fileName) {
+            throw new PluginError(PLUGIN_NAME, 'Missing fileName');
+        } else {
+            return new Buffer(JSON.stringify(result));
+        }
     }
 
     return through.obj(handleStream, endStream);
